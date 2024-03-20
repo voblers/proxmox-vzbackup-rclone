80c45136e9b79430c97b0a8a53379ec0487e32a5
root <root@proxmox-1.vobla-home.net>
2024-03-20 23:24:27 +0200

    Update vzbackup-rclone.sh

--- vzbackup-rclone.sh
+++ vzbackup-rclone.sh
@@ -2,10 +2,11 @@
 # ./vzbackup-rclone.sh rehydrate YYYY/MM/DD file_name_encrypted.bin
 
 ############ /START CONFIG
-drive="gd-backup_crypt"
-dumpdir="/mnt/pve/pvebackups01/dump" # Set this to where your vzdump files are stored
-backups="backups" # Set this to desired cloud folder
-MAX_AGE=3 # This is the age in days to keep local backup copies. Local backups older than this are deleted.
+drive="onedrive_crypt"
+dumpdir="/var/lib/vz/dump" # Set this to where your vzdump files are stored
+backups="" # Set this to desired cloud folder
+
+MAX_AGE=7 # This is the age in days to keep local backup copies. Local backups older than this are deleted.
 MAX_CLOUD_AGE=31 # This is the age in days to keep cloud backup copies. Cloud backups older than this are deleted
 ############ /END CONFIG
 
